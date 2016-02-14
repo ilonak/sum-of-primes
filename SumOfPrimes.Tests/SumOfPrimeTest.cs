1c82a0c1ff98b08abca26ce9fec79a5244d7901d
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-14 09:07:55 -0500

    Implemented IsPrimeNumber, GetSumOfPrimes. Added tests.
    Created first test to check GetSumOfPrimes
    
    Created empty unit test project.
    
    Created empty project.

---
+++ SumOfPrimes.Tests/SumOfPrimeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SumOfPrimes.Tests
+{
+	[TestClass]
+	public class SumOfPrimeTest
+	{
+		[TestMethod]
+		public void GetSumOfPrimes_ForOne_ExpectedSumIsOne()
+		{
+			uint expected = 1;
+			var actual = Program.GetSumOfPrimes(1);
+			Assert.AreEqual(expected, actual, "Wrong sum returned");
+		}
+
+		[TestMethod]
+		public void GetSumOfPrimes_ForFive_ExpectedSumIs18()
+		{
+			uint expected = 18;
+			var actual = Program.GetSumOfPrimes(5);
+			Assert.AreEqual(expected, actual, "Wrong sum returned");
+		}
+
+		[TestMethod]
+		public void IsPrimeNumber_ManyDifferent_ExpectedProperResult()
+		{
+			var data = new List<Tuple<uint,bool>> {
+				new Tuple<uint,bool>(829, true),
+				new Tuple<uint,bool>(2, true),
+				new Tuple<uint,bool>(7, true),
+				new Tuple<uint,bool>(102, false),
+			};
+
+			foreach (var item in data)
+			{
+				var actual = Program.IsPrimeNumber(item.Item1);
+				if (item.Item2)
+					Assert.IsTrue(actual, string.Format("Should be true, {0} is prime number", item.Item1));
+				else
+					Assert.IsFalse(actual, string.Format("Should be false, {0} is NOT prime number", item.Item1));
+			}
+		}
+
+		[TestMethod]
+		public void IsPrimeNumber_For829_ExpectedTrue()
+		{
+			var actual = Program.IsPrimeNumber(829);
+			Assert.IsTrue(actual, "Should be true - 829 is prime number");
+		}
+	}
+}
---
+++ SumOfPrimes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SumOfPrimes
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			uint primeNumbersCount = 1000;
+			uint sumOfPrimes = GetSumOfPrimes(primeNumbersCount);
+					
+			Console.WriteLine("Sum of first {0} primes = {1}", primeNumbersCount, sumOfPrimes);
+			Console.ReadLine();
+		}
+
+		public static uint GetSumOfPrimes(uint primeNumbersCount)
+		{
+			uint found = 0;
+			uint i = 1;
+			uint sumOfPrimes = 0;
+
+			while (found < primeNumbersCount)
+			{
+				if (IsPrimeNumber(i))
+				{
+					sumOfPrimes += i;
+					found++;
+				}
+				i++;
+			}
+			return sumOfPrimes;
+		}
+
+		public static bool IsPrimeNumber(uint number)
+		{
+			bool isPrime = true;
+			if (number != 1)
+			{
+				for (int i = 2; i < number && isPrime; i++)
+				{
+					if (number % i == 0)
+					{
+						isPrime = false;
+					}
+				}
+			}
+			return isPrime;
+		}
+	}
+}
